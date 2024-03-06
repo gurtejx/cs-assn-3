4a47812aaa5853b38963ba6c0967af773c5326d7
Gurtej Malik <gurtejmalik@gmail.com>
2024-03-05 17:39:18 -0800

    Add configuration for entity relationships and inheritence hierarchy

--- comp4945assignment3/AppContext.cs
+++ comp4945assignment3/AppContext.cs
@@ -10,5 +10,32 @@ namespace CS_assn_3
         }
 
         public DbSet<Customer> Customers { get; set; }
+        public DbSet<Employee> Empployees { get; set; }
+        public DbSet<Service> Services { get; set; }
+        
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            // Map inheritance hierarchy
+            modelBuilder.Entity<Person>()
+                .Map<Employee>(m => m.Requires("Discriminator").HasValue("Employee"))
+                .Map<Customer>(m => m.Requires("Discriminator").HasValue("Customer"));
+            
+            // Configure many-to-many relationship between customers and services
+            modelBuilder.Entity<Customer>()
+                .HasMany(c => c.Req_Services)
+                .WithMany(s => s.Customers_Req)
+                .Map(cs =>
+                {
+                    cs.MapLeftKey("Id");
+                    cs.MapRightKey("ServiceId");
+                    cs.ToTable("Customer_Service");
+                });
+            
+            // Configure one-to-many relationship between employees and services
+            modelBuilder.Entity<Employee>()
+                .HasRequired<Service>(e => e.Qualififed_Service)
+                .WithMany(s => s.Emp_Qualified)
+                .HasForeignKey<int>(e => e.Qualififed_Service.SericeId);
+        }
     }
 }
--- comp4945assignment3/Models/Service.cs
+++ comp4945assignment3/Models/Service.cs
@@ -4,7 +4,7 @@ namespace CS_assn_3.Models
 {
     public class Service
     {
-        public int Id { get; set; }
+        public int SericeId { get; set; }
         public string Name { get; set; }
         public ICollection<Employee> Emp_Qualified { get; set; }
         public ICollection<Customer> Customers_Req { get; set; }
